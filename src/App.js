942db0a23e5eee20efde996f6cc311359aac47d3
Francis Macapobre <f.macapobre@gmail.com>
2020-06-12 10:02:36 -0700

    fixed errors. compiles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Visualization from './components/Visualization';
 import {graphSampleData} from './sampleData.js';
@@ -11,7 +10,7 @@ import GraphNode from './models/GraphNode';
 import GraphEdge from './models/GraphEdge';
 
 // User max input is 5
-let mockRepoURLs = [
+var mockRepoURLs = [
   'https://github.com/facebook/react-native',
   'https://github.com/facebook/react',
   'https://github.com/twbs/bootstrap',
@@ -19,8 +18,8 @@ let mockRepoURLs = [
   'https://github.com/facebook/watchman'
 ]
 
-let edges = []
-let nodes = []
+var edges = []
+var nodes = []
 
 // TODO - buildData from user input and hook up to analyze button
 async function buildData() {
@@ -32,65 +31,66 @@ async function buildData() {
 async function buildRepo() {
   for (var i = 0; i < mockRepoURLs.length; i++) {
     const params = mockRepoURLs[i].match(/.*\/(.*)\/(.*)$/)
-    let owner = params[1]
-    let name = params[2]
+    var URLOwner = params[1]
+    var URLName = params[2]
     console.log("buildData URL:", mockRepoURLs[i])
-    let repoData = await fetchRepo(owner, name);
-    let id = repoData["id"]
-    let name = repoData["name"]
-    let owner = repoData["owner"]["login"]
-    let node = new GraphNode(id, name, "repo", owner);
+    var repoData = await fetchRepo(URLOwner, URLName);
+    var id = repoData["id"]
+    var name = repoData["name"]
+    var owner = repoData["owner"]["login"]
+    var node = new GraphNode(id, name, "repo", owner);
     nodes.push(node)
   }
 }
 
 async function buildContributors() {
   for (const node of nodes) {
-    let name = nodes[i].label
-    let owner = nodes[i].title
-    let contributorData = await fetchContributors(owner, name)
-    let contributor;
+    var nodeName = node.label
+    var nodeOwner = node.title
+    var contributorData = await fetchContributors(nodeOwner, nodeName)
+    var id, name, contributions, contributorNode;
     if (contributorData.length < 20) {
       for (const contributor of contributorData) {
-        let id = contributor["id"]
-        let name = contributor["login"]
-        let contributions = contributor["contributions"]
-        contributor = new GraphNode(id, name, "user", contributions)
+        id = contributor["id"]
+        name = contributor["login"]
+        contributions = contributor["contributions"]
+        contributorNode = new GraphNode(id, name, "user", contributions)
       }
     } else {
       for (var i = 0; i < 20; i++) {
-        let id = contributorData[i]["id"]
-        let name = contributorData[i]["login"]
-        let contributions = contributorData["contributions"]
-        contributor = new GraphNode(id, name, "user", contributions)
+        id = contributorData[i]["id"]
+        name = contributorData[i]["login"]
+        contributions = contributorData["contributions"]
+        contributorNode = new GraphNode(id, name, "user", contributions)
       }
     }
-    let edge = new GraphEdge(contributor.id, node.id, 'contributor')
-    nodes.push(contributor)
+    var edge = new GraphEdge(contributorNode.id, node.id, 'contributor')
+    nodes.push(contributorNode)
     edges.push(edge)
   }
 }
 
 async function buildFollowers() {
-  let users = []
+  var users = []
   for (const node in nodes) {
-    if (node.type == 'user') {
+    if (node.type === 'user') {
       users.push(node)
     }
   }
 
   for (var i = 0; i < users.length; i++) {
     for (var j = 0; j < users.length; j++) {
-      if (i == j) return
+      if (i === j) return
       else {
-        isFollowing_j = await fetchFollowing(users[i].label, users[j].label)
+        var isFollowing_j = await fetchFollowing(users[i].label, users[j].label)
         if (isFollowing_j) {
-          let edge = new GraphEdge(users[i].id, users[j].id, 'friend')
+          var edge = new GraphEdge(users[i].id, users[j].id, 'friend')
           edges.push(edge)
         }
       }
     }
   }
+
 }
 
 export default class App extends Component {
